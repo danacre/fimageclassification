09ddf088f21dadab2d2064110dfc0d46c1c6e4d7
wlcl2001 <wizliechulok@gmail.com>
2020-06-19 23:48:24 +0800

    my latest

--- app/src/main/java/com/fypic/imageclassification/Object.java
+++ app/src/main/java/com/fypic/imageclassification/Object.java
@@ -1,28 +1,34 @@
 package com.fypic.imageclassification;
 
+import java.time.DateTimeException;
+import java.util.Date;
+
 public class Object {
     private int matid;
     private int id;
     private String mat;
+    private Date date;
+
 
     public Object(int id, int matid) {
         this.matid = matid;
         this.mat = mat;
         this.id = id;
+        this.date = date;
 
-        if(matid == 1){
+        if (matid == 1) {
             mat = "Paper";
         }
 
-        if(matid == 2){
+        if (matid == 2) {
             mat = "Metal";
         }
 
-        if(matid == 3){
+        if (matid == 3) {
             mat = "Plastic";
         }
 
-        if(matid == 4){
+        if (matid == 4) {
             mat = "Waste";
         }
     }
@@ -34,16 +40,5 @@ public class Object {
     public void setMat(String mat) {
         this.mat = mat;
     }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getMatId() {
-        return matid;
-    }
 }
+
